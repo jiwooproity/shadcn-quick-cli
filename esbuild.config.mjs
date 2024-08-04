45a1e65dfafae902fb500321839f8190963d2361
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-08-04 13:16:06 +0900

    Chore: change esbuild config and version

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -3,8 +3,8 @@ import { build } from "esbuild";
 build({
   entryPoints: ["./src/index.ts"],
   bundle: true,
+  target: "ES5",
   outdir: "./build",
   platform: "node",
   minify: true,
-  metafile: true,
 });

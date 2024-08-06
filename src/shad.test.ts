95966c05242ce0d8183158a6804350f56ca8b1fa
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-08-06 14:28:45 +0900

    Chore: Create new test file, components/ui for test shad CLI

--- src/shad.test.ts
+++ src/shad.test.ts
@@ -1,72 +1,20 @@
 import { chrowingHTML, getComponents, isNeedOverwriting, readdir, start } from "./shad";
 import { test, expect } from "@jest/globals";
 
-const shadcnUIList = [
-  "Accordion",
-  "Alert",
-  "Alert Dialog",
-  "Aspect Ratio",
-  "Avatar",
-  "Badge",
-  "Breadcrumb",
-  "Button",
-  "Calendar",
-  "Card",
-  "Carousel",
-  "ChartNew",
-  "Checkbox",
-  "Collapsible",
-  "Combobox",
-  "Command",
-  "Context Menu",
-  "Data Table",
-  "Date Picker",
-  "Dialog",
-  "Drawer",
-  "Dropdown Menu",
-  "Form",
-  "Hover Card",
-  "Input",
-  "Input OTP",
-  "Label",
-  "Menubar",
-  "Navigation Menu",
-  "Pagination",
-  "Popover",
-  "Progress",
-  "Radio Group",
-  "Resizable",
-  "Scroll Area",
-  "Select",
-  "Separator",
-  "Sheet",
-  "Skeleton",
-  "Slider",
-  "Sonner",
-  "Switch",
-  "Table",
-  "Tabs",
-  "Textarea",
-  "Toast",
-  "Toggle",
-  "Toggle Group",
-  "Tooltip",
-];
-
 describe("Verify that the scan directory is valid.", () => {
   test("Read in directory files.", () => {
-    expect(readdir("./test")).toEqual(["hello.txt", "next-test", "ui"]);
+    expect(readdir("./test")).toEqual(["lib", "ui"]);
   });
 });
 
-describe("Verify that the Chrowing is valid.", () => {
+describe("Verify that the crawling is valid.", () => {
   test("GET request is valid to shadcn/ui website ( status: 200 )", async () => {
     const html = await chrowingHTML();
     expect(html.status).toBe(200);
   });
 
-  test("Chrowing in shadcn/ui menu.", async () => {
-    expect(await getComponents().then((res) => res)).toEqual(shadcnUIList);
+  test("Crawling in shadcn/ui menu.", async () => {
+    expect(await getComponents().then((res) => res)).toHaveLength(49);
   });
 });
 

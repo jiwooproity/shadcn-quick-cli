865a864a363711b30a8b445e608a9ec0ab4b809b
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-08-06 11:48:37 +0900

    Fix: fix omit build file

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { program } from "commander";
 import { start } from "./shad";
 
 program
-  .version("v1.5.3", "-v, --version", "output the current version")
+  .version("v1.5.4", "-v, --version", "output the current version")
   .option("-t, --target <component>", "directly select to shadcn-ui component")
   .option("-s, --select", "get component list for select")
   .option("-o, --overwrite", "overwrite the existing component and download a new one")
